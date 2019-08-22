e1ef2267ca85daf4169bfa5d1e48c029b139427c
Zackary Cowled <ZackaryCowled10010@gmail.com>
2019-08-22 23:13:55 +1000

    Build  V-0.0.2

---
+++ precache-manifest.498d585afe3f64cf028bea300da2c99a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7c50350d7cfcba2bd9d86a2a618d85b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6918c696d2728c523ba",
+    "url": "/static/css/main.0a7747bb.chunk.css"
+  },
+  {
+    "revision": "58e6b6fd0b1928f29129",
+    "url": "/static/js/2.93c36f9c.chunk.js"
+  },
+  {
+    "revision": "c6918c696d2728c523ba",
+    "url": "/static/js/main.96b79b65.chunk.js"
+  },
+  {
+    "revision": "ade0eb8e9b595c6acf5a",
+    "url": "/static/js/runtime~main.cb788e84.js"
+  }
+]);
